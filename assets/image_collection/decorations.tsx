289eda3891dea0d2831aeb4afd586bcf5c954f38
KrLiam <88288206+KrLiam@users.noreply.github.com>
2026-02-14 14:15:04 -0300

    add 'image_collection' example

---
+++ assets/image_collection/decorations.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="decorations" tilewidth="32" tileheight="64" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" x="0" y="0" width="32" height="64">
+  <image source="decorations.png" width="128" height="128"/>
+ </tile>
+ <tile id="1" x="32" y="48" width="16" height="16">
+  <image source="decorations.png" width="128" height="128"/>
+ </tile>
+ <tile id="2" x="64" y="32" width="32" height="32">
+  <image source="decorations.png" width="128" height="128"/>
+ </tile>
+</tileset>
